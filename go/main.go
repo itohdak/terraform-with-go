278dba8974b6e54c4144fc4694b3490287374016
itohdak <itohdak@gmail.com>
2021-08-09 12:20:59 +0000

    add module for sending slack DM

--- go/main.go
+++ go/main.go
@@ -13,7 +13,10 @@ import (
 	"github.com/hashicorp/terraform-exec/tfexec"
 	"github.com/hashicorp/terraform-exec/tfinstall"
 	"github.com/labstack/echo"
+	"github.com/slack-go/slack"
 )
+
+var api *slack.Client
 
 type CreateResourceRequest struct {
 	// TODO: these are only examples. please modify to fit your needs.
@@ -131,8 +134,16 @@ func postHandler(c echo.Context) error {
 	if err := c.Bind(req); err != nil {
 		return err
 	}
-	go createNewResource("/home/ec2-user/" + req.Username)
-	return c.String(http.StatusCreated, fmt.Sprintf("Creating new resource for %s", req.Username))
+	// go createNewResource("/home/ec2-user/" + req.Username)
+	userID := "UFECAQTHC" // TODO
+	if err := sendSlackDM(
+		api,
+		userID,
+		slack.MsgOptionText("This is a DM", true),
+	); err != nil {
+		return err
+	}
+	return c.String(http.StatusCreated, fmt.Sprintf("Creating new resource for %s\n", req.Username))
 }
 func deleteHandler(c echo.Context) error {
 	req := new(DeleteResourceRequest)
@@ -140,18 +151,44 @@ func deleteHandler(c echo.Context) error {
 		return err
 	}
 	go destroyResource("/home/ec2-user/" + req.Username)
-	return c.String(http.StatusOK, fmt.Sprintf("Removing resource for %s", req.Username))
+	return c.String(http.StatusOK, fmt.Sprintf("Removing resource for %s\n", req.Username))
+}
+func sendSlackDM(client *slack.Client, userID string, options slack.MsgOption) error {
+	param := new(slack.OpenConversationParameters)
+	param.Users = append(param.Users, userID)
+	channel, _, _, err := client.OpenConversation(param)
+	if err != nil {
+		return err
+	}
+	channelID := channel.GroupConversation.Conversation.ID
+	_, _, err = client.PostMessage(channelID, options)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 func main() {
 	// err := printContent("/home/ec2-user/template/main.tf")
 	// if err != nil {
 	// 	log.Fatalf("error in printContent")
 	// }
+
+	token, ok := os.LookupEnv("SLACK_TOKEN")
+	if !ok {
+		fmt.Println("Missing SLACK_TOKEN in environment")
+		os.Exit(1)
+	}
+	api = slack.New(
+		token,
+		slack.OptionDebug(true),
+		slack.OptionLog(log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)),
+	)
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!!")
 	})
-	e.POST("/create_new_resource", postHandler)
-	e.DELETE("/delete_resource", deleteHandler)
+	e.POST("/instance/create", postHandler)
+	e.DELETE("/instance/delete", deleteHandler)
 	e.Logger.Fatal(e.Start(":8080"))
 }
